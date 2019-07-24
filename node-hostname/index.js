b16e5633adb2a3ef33c163af686f32a64d02f959
Robbie Hickey <Robbie.Hickey@alliedelec.com>
2019-07-24 09:40:51 -0500

    simulating startup time and adding healthcheck endpoint

--- node-hostname/index.js
+++ node-hostname/index.js
@@ -2,11 +2,29 @@ let os = require('os');
 let http = require('http');
 
 let server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Requested processed on host ' + os.hostname());
-  res.end();
+
+  console.log('Received http request for ' + req.url);
+
+  // simulate health check endpoint
+  if (req.url.toLowerCase() === "/healthz") {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ status: "OK" }))
+    res.end();
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('V2: Request processed on host ' + os.hostname());
+    res.end();
+  }
 });
 
-let port = 80;
-console.log('Listening on port ' + port);
-server.listen(port);
+// allow env to override default port
+let port = process.env.APP_PORT || 80;
+
+// simulate application startup delay
+console.log('Application starting up...')
+
+setTimeout(() => {
+  server.listen(port, () => {
+    console.log('Application listening on port ' + port);
+  });
+}, 10000)
